7ea4f74e56d3a97cfabcbe612b6682778082e4e0
Goldy <112779392+Goldypatel@users.noreply.github.com>
2024-06-15 22:42:57 +0530

    Update ErrorH.sol

--- ETH intermidiate -Error_Handling/ErrorH.sol
+++ ETH intermidiate -Error_Handling/ErrorH.sol
@@ -1,38 +1,41 @@
-
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity ^0.8.17;
-
-contract ErrorH {
-    address owner ;
-    uint public num;
+contract banksys {
+    address public owner;
 
     constructor(){
-        owner = msg.sender;
+         owner = msg.sender ;
+    }
+    mapping(address => uint) public balances;
+
+    function deposit(uint256 value) public {
+        require(msg.sender==owner, "make sure you deposit in your account");
+        balances[msg.sender] += value;
+
+        
+        assert(balances[msg.sender] >= value); //to update balances corrreeclty
     }
 
+    function withdraw(uint amount) public {
+        require(amount <= balances[msg.sender], "balance not enough");
+        require(msg.sender==owner,"you are not the owner.");
 
-//code for require statement------------------------------------------------------------------- 
-//bool / takes two para
-    function checkerror1() public {
-        num++;
-        require(msg.sender==owner, "you are not the owner");   
-    }
-
-
-//code for assert statement------------------------------------------------------------------- 
-// bool /used to check for internal errors
-function checkerror2() public {  
-        num++;
-        assert(msg.sender==owner);
-    }
-
-
-//code for revert statement------------------------------------------------------------------- 
-    function checkerror3() public{
-        num++;
-        if(msg.sender != owner){
-        revert("Make sure you are the owner! ");
+        balances[msg.sender] -= amount;
+        bool success = false;
+        if (amount > 0) {
+            success = true;
         }
+
+        if (!success) {
+            balances[msg.sender] += amount; 
+            revert("Withdrawal failed.");
+        }
+
+        assert(balances[msg.sender] >= 0);
+    }
+
+    function checkbalance() public view returns (uint) {
+        return balances[msg.sender];
     }
 }
